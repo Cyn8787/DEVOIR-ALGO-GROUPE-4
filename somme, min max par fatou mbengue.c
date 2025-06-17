8b2bdf75e574ff44af59eda24bb121b38a7078d9
Cyn8787 <cynthiadiadhiou@gmail.com>
2025-06-17 20:07:43 +0000

    Create somme, min max par fatou mbengue.c

---
+++ somme, min max par fatou mbengue.c
@@ -0,0 +1,35 @@
+//
+float sommemontant(struct facture tab[N] )
+int i;
+{
+    float somme = 0;
+    for (i = 0; i < N; i++)
+    {
+        somme += Tab[i].montant;
+    }
+    return somme;
+}
+float minmontant(struct facture tab[N])
+{
+    float min = tab[0].montant;
+    for (int i = 1; i < N; i++)
+    {
+        if (tab[i].montant < min)
+        {
+            min = tab[i].montant;
+        }
+    }
+    return min;
+}
+float maxmontant(struct facture tab[N])
+{
+    float max = tab[0].montant;
+    for (int i = 1; i < N; i++)
+    {
+        if (tab[i].montant > max)
+        {
+            max = tab[i].montant;
+        }
+    }
+    return max;
+}
