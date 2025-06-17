0dff2dd78a25d5b1e1e1dddfddda67038c7f2c25
Adja2004 <khadijaarassoul5@gmail.com>
2025-06-17 15:25:27 +0000

    Update definir;procedure remplirTab ... par Adja Dieng.c

--- definir;procedure remplirTab ... par Adja Dieng.c
+++ definir;procedure remplirTab ... par Adja Dieng.c
@@ -4,18 +4,28 @@
 struct facture{
 int idetudiant; char[6]objet;char[10]numero;float montant;
 }  
-//Procedure remplirTab
-void remplirTab(struct facture tab[N]){ 
+//Procedure remplirTab par Adja Dieng
+void remplirTab(struct facture tab[N]){
 int i;
 for(i=0;i<N;i++){
-printf("sasir idEtudiant");
-scanf(%.d"&Tab[i].id");
+printf("saisir l'idetudiant");
+scanf("%d,&tab[i].idetudiant");
 printf("saisir l'objet");
-scanf(%s,&p.objet);
+scanf("%s,&tab[i].objet");
 printf("saisir le numero");
-scanf(%s,&p.numero);
+scanf("%d,&tab[i].numero");
 printf("saisir le montant");
-scanf(%.d,&p)
+scanf("%f,&tab[i].montant"); 
+}
+//Procedure afficherTab par Adja Dieng
+void afficherTab(struct facture tab[N]){
+ int i;
+for(i=0; i<N;i++){
+printf("la liste des facture est:%d %f %s %d",tab[i].idetudiant ,tab[i].objet ,tab[i].numero ,tab[i].montant);
 
 
 }
+}
+
+}
+
