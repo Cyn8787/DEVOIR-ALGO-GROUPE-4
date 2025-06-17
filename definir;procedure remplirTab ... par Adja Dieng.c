941f6da80b9f087aa7049956770aa485c718a7cf
Adja2004 <khadijaarassoul5@gmail.com>
2025-06-17 14:22:39 +0000

    Update definir;procedure remplirTab ... par Adja Dieng.c

--- definir;procedure remplirTab ... par Adja Dieng.c
+++ definir;procedure remplirTab ... par Adja Dieng.c
@@ -1,6 +1,21 @@
-//definir la structure facture
-#include.stdio.h
+//definir la structure facture et etudiant par Adja Dieng
+#include<stdio.h>
+#include<string.h>
 struct facture{
-int id;
+int idetudiant; char[6]objet;char[10]numero;float montant;
+}  
+//Procedure remplirTab
+void remplirTab(struct facture tab[N]){ 
+int i;
+for(i=0;i<N;i++){
+printf("sasir idEtudiant");
+scanf(%.d"&Tab[i].id");
+printf("saisir l'objet");
+scanf(%s,&p.objet);
+printf("saisir le numero");
+scanf(%s,&p.numero);
+printf("saisir le montant");
+scanf(%.d,&p)
+
 
 }
