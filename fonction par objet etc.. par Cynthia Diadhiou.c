9b00e17d19dfc353a55d491579aa15fb45b83d30
Cyn8787 <cynthiadiadhiou@gmail.com>
2025-06-17 13:58:23 +0000

    Update fonction par objet etc.. par Cynthia Diadhiou.c

--- fonction par objet etc.. par Cynthia Diadhiou.c
+++ fonction par objet etc.. par Cynthia Diadhiou.c
@@ -7,5 +7,11 @@ sommeparobjet= somme+tab[i].montant;
    }
    return sommeparobjet;
 }
-//Procédure factureparétudiant par Cynthia Murielle Diadhiou
-void  afficherfactureparétudiant(str)
+//Procédure recherche par id et factureparétudiant par Cynthia Murielle Diadhiou
+void  afficherfactureparétudiant(struct facture tab[N]){
+    int r;
+    printf("veuillez saisir l'id de l'étudiant");
+    scanf("%d",&r);
+    etudiant=fopen("etudiant.txt","r+");
+    while(!feof(personne,"%d %s %s %s", &tab[i].idetudiant,))
+}
